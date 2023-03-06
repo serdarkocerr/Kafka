d43cb49d1caa2ee62c7083d26bf40abbfaae7598
serdarkocerr <49731780+serdarkocerr@users.noreply.github.com>
2023-03-07 00:03:31 +0300

    Update kafka-del-topic.sh

--- kafka-del-topic.sh
+++ kafka-del-topic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper 10.152.16.201:2181 --list )
+TOPICS=$(/opt/kafka/bin/kafka-topics.sh --zookeeper 127.0.0.1:2181 --list )
 
 for T in $TOPICS
 do
   if [ "$T" != "__consumer_offsets" ]; then
-   /opt/infra/kafka/bin/kafka-topics.sh --zookeeper 10.152.16.201:2181 --delete --topic $T
+   /opt/infra/kafka/bin/kafka-topics.sh --zookeeper  127.0.0.1:2181 --delete --topic $T
   fi
 done
